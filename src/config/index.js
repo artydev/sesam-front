fb107d7a1d4c21a27a2f078e8a5ca0ec5e5ed0f7
Antoine Paris <antoineparis77@gmail.com>
2019-07-17 12:29:18 +0200

    config

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,11 @@
-//var url_prefix = 'https://Admin:dtymoney91190@m.dgccrf.rie.gouv.fr/couchdb';
-var url_prefix = 'http://Admin:password@172.17.64.136:5984';
+var url_prefix = 'https://Admin:dtymoney91190@m.dgccrf.rie.gouv.fr/couchdb';
+//var url_prefix = 'http://Admin:password@172.17.64.136:5984';
 
 export default {
   replication_starting_hour: 9, // at 9am every morning the new data is here we have to refetch it
   backend: {
-    base_url: 'http://localhost:8080'
-    //base_url: '/data'
+    //base_url: 'http://localhost:8080'
+    base_url: '/data'
   },
   couchDb: {
     url_dossiers: url_prefix + '/dossiers',
